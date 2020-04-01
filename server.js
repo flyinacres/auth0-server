9d195ffbf90d809be389316bf487f42eed4e49d7
flyinacres <ron.fischer@scienceexchange.com>
2020-03-31 20:11:23 -0700

    testing

--- server.js
+++ server.js
@@ -4,13 +4,83 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const port = 8000;
+const jwt = require("express-jwt"); // NEW
+const jwksRsa = require("jwks-rsa"); // NEW
+
+// Set up Auth0 configuration 
+const authConfig = {
+  domain: "dev-aaqyyzhg.auth0.com",
+  audience: "https://rff-vue-express-api.com"
+};
+
+// NEW
+// Create middleware to validate the JWT using express-jwt
+const checkJwt = jwt({
+  // Provide a signing key based on the key identifier in the header and the signing keys provided by your Auth0 JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+  }),
+
+  // Validate the audience (Identifier) and the issuer (Domain).
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithm: ["RS256"]
+});
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
+ // mock data to send to our frontend
+let events = 
+[
+  {
+    id: 1,
+    name: 'Charity Ball',
+    category: 'Scamming',
+    description: 'Spend an elegant night of dinner and dancing with us as we raise money for our new rescue farm.',
+    featuredImage: 'https://placekitten.com/500/500',
+    images: [
+      'https://placekitten.com/500/500',
+      'https://placekitten.com/500/500',
+      'https://placekitten.com/500/500',
+    ],
+    location: '1234 Meows Ave',
+    date: '12-25-2019',
+    time: '11:30'
+  },
+  {
+    id: 2,
+    name: 'Rescue Center Goods Drive',
+    category: 'Adoptions',
+    description: 'Come to our donation drive to help us replenish our stock of pet food, toys, bedding, etc. We will have live bands, games, food trucks, and much more.',
+    featuredImage: 'https://placekitten.com/500/500',
+    images: [
+      'https://placekitten.com/500/500'
+    ],
+    location: '1234 Dog Alley',
+    date: '11-21-2019',
+    time: '12:00'
+  }
+];
+
 app.get('/', (req, res) => {
   res.send(`Hi! Server is listening on port ${port}`)
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
+});
+
+// NEW (updated)
+// For this app, we only want to protect the route that returns the details of an event
+app.get('/events/:id', checkJwt, (req, res) => {
+  const id = Number(req.params.id);
+  const event = events.find(event => event.id === id);
+  res.send(event);
 });
 
 // listen on the port
